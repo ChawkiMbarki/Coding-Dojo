af39b5b39e9ea9f0797580b3a993416d107f211d
ChawkiMbarki <mbarkishawki@outlook.com>
2024-03-01 08:31:04 +0100

    Add Friends through FROM

--- Modules/Python/Flask and MySQL/Practice/MySQL Connection Errors/server.py
+++ Modules/Python/Flask and MySQL/Practice/MySQL Connection Errors/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 # import the class from friend.py
 from friend import Friend
 app = Flask(__name__)
@@ -7,6 +7,16 @@ def index():
     # call the get all classmethod to get all friends
     friends = Friend.get_all()
     return render_template("index.html", friends = friends)
-            
+
+@app.route("/create_friend", methods=["POST"])
+def create_friend():
+    data = {
+        'fname': request.form["fname"],
+        'lname': request.form["lname"],
+        'occ': request.form["occ"]
+    }
+    Friend.save(data)
+    return redirect("/")
+
 if __name__ == "__main__":
     app.run(debug=True)
