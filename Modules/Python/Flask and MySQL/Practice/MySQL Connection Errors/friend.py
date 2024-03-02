f669cc19fe165bc36c15bbc688a34973930b2ca0
ChawkiMbarki <mbarkishawki@outlook.com>
2024-03-02 12:39:34 +0100

    Add the update functionality
    Now friends can be updated

--- Modules/Python/Flask and MySQL/Practice/MySQL Connection Errors/friend.py
+++ Modules/Python/Flask and MySQL/Practice/MySQL Connection Errors/friend.py
@@ -2,7 +2,8 @@
 from mysqlconnection import connectToMySQL
 # model the class after the friend table from our database
 class Friend:
-    def __init__( self , data ):
+    DB = "first_flask"
+    def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
@@ -14,7 +15,7 @@ class Friend:
     def get_all(cls):
         query = "SELECT * FROM friends;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
-        results = connectToMySQL('first_flask').query_db(query)
+        results = connectToMySQL(cls.DB).query_db(query)
         # Create an empty list to append our instances of friends
         friends = []
         # Iterate over the db results and create instances of friends with cls.
@@ -27,4 +28,11 @@ class Friend:
     def save(cls, data):
         query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(fname)s, %(lname)s, %(occ)s, NOW(), NOW())"
         # data is a dictionary that will be passed into the save method from server.py
-        return connectToMySQL('first_flask').query_db(query, data)
+        return connectToMySQL(cls.DB).query_db(query, data)
+    
+    @classmethod
+    def update(cls,data):
+        query = """UPDATE friends 
+                SET first_name=%(fname)s,last_name=%(lname)s, occupation=%(occ)s 
+                WHERE id = %(id)s;"""
+        return connectToMySQL(cls.DB).query_db(query,data)
