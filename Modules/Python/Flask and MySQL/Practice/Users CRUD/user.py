4a3e0d700a22a05a5683de0704f46f6a140cff6e
ChawkiMbarki <mbarkishawki@outlook.com>
2024-03-31 05:30:15 +0200

    Fix "INSERT" and "UPDATE" queries

--- Modules/Python/Flask and MySQL/Practice/Users CRUD/user.py
+++ Modules/Python/Flask and MySQL/Practice/Users CRUD/user.py
@@ -7,7 +7,7 @@ class User:
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
-        self.occupation = data['occupation']
+        self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
@@ -23,10 +23,15 @@ class User:
             users.append(cls(user))
         return users
     
+    @classmethod
+    def get_user(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        return cls(connectToMySQL(cls.DB).query_db(query, data)[0])
+    
     # class method to save our user to the database
     @classmethod
     def save(cls, data):
-        query = "INSERT INTO users (first_name, last_name, occupation, created_at, updated_at) VALUES (%(fname)s, %(lname)s, %(occ)s, NOW(), NOW())"
+        query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES (%(fname)s, %(lname)s, %(email)s, NOW(), NOW())"
         # data is a dictionary that will be passed into the save method from server.py
         return connectToMySQL(cls.DB).query_db(query, data)
     
@@ -34,7 +39,7 @@ class User:
     def update(cls,data):
         query = """
                 UPDATE users 
-                SET first_name=%(fname)s,last_name=%(lname)s, occupation=%(occ)s 
+                SET first_name=%(fname)s,last_name=%(lname)s, email=%(email)s, updated_at=NOW()
                 WHERE id = %(id)s;
         """
         return connectToMySQL(cls.DB).query_db(query,data)
