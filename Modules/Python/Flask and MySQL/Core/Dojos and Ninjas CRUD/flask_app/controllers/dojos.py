648e6fdcbb009079f8b26ff8083c5f1003957564
ChawkiMbarki <mbarkishawki@outlook.com>
2024-04-01 09:03:27 +0200

    Dojo's page route and template

--- Modules/Python/Flask and MySQL/Core/Dojos and Ninjas CRUD/flask_app/controllers/dojos.py
+++ Modules/Python/Flask and MySQL/Core/Dojos and Ninjas CRUD/flask_app/controllers/dojos.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for
 from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import dojo
 
 @app.route("/")
 def root():
@@ -9,3 +10,7 @@ def root():
 @app.route("/dojos")
 def home():
   return render_template("index.html")
+
+@app.route("/dojos/<int:id>")
+def show_dojo(id):
+  return render_template('dojo.html', dojo = 1) # dojo.get(id)
