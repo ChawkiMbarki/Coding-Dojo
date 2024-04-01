5a41fec42e90dec06dc6aa3e6a0f34a498e269dc
ChawkiMbarki <mbarkishawki@outlook.com>
2024-04-01 11:07:15 +0200

    Add, Fetch and Use data from the database

--- Modules/Python/Flask and MySQL/Core/Dojos and Ninjas CRUD/flask_app/controllers/dojos.py
+++ Modules/Python/Flask and MySQL/Core/Dojos and Ninjas CRUD/flask_app/controllers/dojos.py
@@ -1,7 +1,8 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
-from flask_app.models import dojo
+from flask_app.models.dojo import Dojo
+from flask_app.models.ninja import Ninja
 
 @app.route("/")
 def root():
@@ -9,8 +10,16 @@ def root():
 
 @app.route("/dojos")
 def home():
-  return render_template("index.html")
+  dojos = Dojo.get_all()
+  return render_template("index.html", dojos = dojos)
+
+@app.route("/create_dojo", methods = ['POST'])
+def create_dojo():
+  Dojo.save(request.form)
+  return redirect(url_for('home'))
 
 @app.route("/dojos/<int:id>")
 def show_dojo(id):
-  return render_template('dojo.html', dojo = 1) # dojo.get(id)
+  dojo = Dojo.get(id)
+  ninjas = Ninja.dojo_ninjas(id)
+  return render_template('dojo.html', dojo = dojo, ninjas = ninjas)
