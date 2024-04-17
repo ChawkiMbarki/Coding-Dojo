ad463eb25adb3b59b391126ab5095d0296f35e4a
ChawkiMbarki <mbarkishawki@outlook.com>
2024-04-17 18:32:23 +0200

    Add trip validation

--- Modules/Python/Belt Exam/flask_app/models/validation.py
+++ Modules/Python/Belt Exam/flask_app/models/validation.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 def validate_name(name):
   NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
@@ -17,3 +18,28 @@ def validate_password(password):
     return False #("Password - Must be At least 8 characters")
   return True
 
+def validate_destination(destination):
+  if(len(destination) < 3): 
+    return False #("Destination - Must be At least 3 characters")
+  return True
+
+def validate_start_date(start_date):
+  try:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        current_date = datetime.now()
+        return start_date >= current_date
+  except ValueError:
+        return False #("Start Date - Must be in the future")
+
+def validate_end_date(start_date, end_date):
+  try:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        return end_date >= start_date
+  except ValueError:
+        return False #("End Date - Must be after the start date")
+
+def validate_plan(plan):
+  if(len(plan) == 0): 
+    return False #("Plan - Must be provided!")
+  return True
