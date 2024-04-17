163fcdd210add8a1e3fea3227d1a34b17b8e1128
ChawkiMbarki <mbarkishawki@outlook.com>
2024-04-17 12:13:35 +0200

    Add user's validation

--- Modules/Python/Belt Exam/flask_app/controllers/users.py
+++ Modules/Python/Belt Exam/flask_app/controllers/users.py
@@ -14,7 +14,16 @@ def home():
 
 @app.route('/user/login', methods = ['POST'])
 def login():
+  errors = User.validate_login(request.form)
+
+  if(errors):
+    return redirect(url_for('home'))
   user = User.get_by_email({"email": request.form['login_email']})
+  if(not bcrypt.check_password_hash(user.password, request.form['login_password'])): 
+            error = "Password or Email is wrong"
+            errors.append(error)
+            flash(error, 'error')
+            return redirect(url_for('home'))
   session['id'] = user.id
   return redirect(url_for('dashboard'))
 
@@ -25,6 +34,9 @@ def logout():
 
 @app.route('/user/create', methods = ['POST'])
 def new_user():
+  errors = User.validate_registration(request.form)
+  if(errors):
+    return redirect(url_for('home'))
   password = bcrypt.generate_password_hash(request.form['password'])
   data = {
     'first_name' : request.form['first_name'],
