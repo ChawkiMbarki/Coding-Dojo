07158d15075fa9d606654f66b624b77d18fa80ec
ChawkiMbarki <mbarkishawki@outlook.com>
2024-06-08 05:02:25 +0100

    Add loading for fun

--- Modules/MERN/React/core/axios-pokemon-api/src/App.js
+++ Modules/MERN/React/core/axios-pokemon-api/src/App.js
@@ -6,20 +6,25 @@ import './app.css'
 
 const App = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchAllPokemons = async () => {
     let url = "https://pokeapi.co/api/v2/pokemon?limit=807";
+    setLoading(true);
     try {
       const response = await axios.get(url);
       setPokemons(response.data.results);
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className='app flex column'>
       <button onClick={fetchAllPokemons} disabled={loading}>
+        {loading ? 'Loading...' : 'Fetch Pokemon'}
       </button>
       <Cards pokemons={pokemons} />
     </div>
