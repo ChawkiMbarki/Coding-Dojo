78bf5a9f8eb3695b904f0abc5d5e68dc9a0e27c6
ChawkiMbarki <mbarkishawki@outlook.com>
2024-06-10 04:17:45 +0100

    Create Input Component

---
+++ Modules/MERN/React/core/more-forms/src/components/input/Input.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+
+import MyContext from '../../MyContext';
+import './input.css';
+
+const Input = (props) => {
+  const { setDetails } = useContext(MyContext);
+
+  const handleChange = (event) => {
+    setDetails(prevDetails => ({
+      ...prevDetails,
+      [event.target.id]: event.target.value
+    }));
+  };
+
+  return (
+    <div className='row flex'>
+      <label htmlFor={props.id}>{props.lable}</label>
+      <input id={props.id} type={props.type} onChange={handleChange} />
+    </div>
+  );
+};
+
+export default Input;
