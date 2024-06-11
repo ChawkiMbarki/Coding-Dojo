2983673e9c9072c6b76cad1397d8f4d0852b81eb
ChawkiMbarki <mbarkishawki@outlook.com>
2024-06-11 02:20:11 +0100

    Project Initialization

---
+++ Modules/MERN/React/core/luke-api-walker/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <div className='app flex'>
+      App
+    </div>
+  )
+}
+
+export default App
