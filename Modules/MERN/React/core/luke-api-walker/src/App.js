4a2adbdbf189a264993865f36941fdd68ae2c79d
ChawkiMbarki <mbarkishawki@outlook.com>
2024-06-12 07:05:55 +0100

    Create App component

--- Modules/MERN/React/core/luke-api-walker/src/App.js
+++ Modules/MERN/React/core/luke-api-walker/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
+import { Search, Details, Error } from './components';
 import './app.css';
 
 const App = () => {
-  return (
-    <div className='app flex'>
-      App
-    </div>
-  )
-}
+  const [error, setError] = useState('');
 
-export default App
+  return (
+    <div className='app flex column'>
+      <Search setError={setError} />
+      {error
+        ? (<Error error={error} />)
+        : (<Routes><Route path="/:category/:id" element={<Details setError={setError} />} /></Routes>)
+      }
+    </div>
+  );
+};
+
+export default App;
