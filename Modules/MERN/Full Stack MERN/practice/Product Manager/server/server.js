99c08333fa2fedf19e578afce1d2386680701698
ChawkiMbarki <mbarkishawki@outlook.com>
2024-06-26 01:45:15 +0100

    Server

---
+++ Modules/MERN/Full Stack MERN/practice/Product Manager/server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+require('./config/mongoose.config')
+const routes = require('./routes/product.routes');
+
+const port = process.env.PORT;
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+routes(app)
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
