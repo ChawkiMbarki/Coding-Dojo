d9388953ca99b435f1cfc07d0fcfa2373a3fd3af
ChawkiMbarki <mbarkishawki@outlook.com>
2024-06-26 10:53:28 +0100

    Import previous assignment's files

---
+++ Modules/MERN/Full Stack MERN/core/Product Manager/client/src/views/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductForm from '../components/ProductsForm';
+
+const Main = () => {
+  return (
+    <div>
+      <center>
+        <ProductForm />
+      </center>
+    </div>
+  )
+}
+
+export default Main
