b711a69a7eb7cfa2b54efe9ea666cbd5dff04b01
ChawkiMbarki <mbarkishawki@outlook.com>
2024-06-27 14:18:47 +0100

    Client: Add DELETE Feature

--- Modules/MERN/Full Stack MERN/core/Product Manager/client/src/components/ProductsDisplay.jsx
+++ Modules/MERN/Full Stack MERN/core/Product Manager/client/src/components/ProductsDisplay.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 const ProductsDisplay = (props) => {
+
+  const navigate = useNavigate()
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:8000/api/product/${id}`)
+      .then( res => console.log(res.data))
+      .catch( error => console.error(error))
+    
+    props.setStatus(true)
+  }
 
   return (
     <div>
       <h2>All products</h2>
-      <ul>
+      <ul className='flex column'>
         {props.products.map((product, i) => (
-          <li key={i}><a href={'/product/'+ product._id}>{product.title}</a></li>
+          <li key={i} className='flex'>
+            <Link className='link'  to={'/product/'+ product._id}>{product.title}</Link>
+            <div className="buttons flex">
+              <button className='delete' onClick={() => handleDelete(product._id)}>Delete</button>
+              <button className='update' onClick={() => navigate(`/product/${product._id}/edit`)}>Update</button>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
