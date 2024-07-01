292cdf697f3342339319e7091e1102c98325e40b
ChawkiMbarki <mbarkishawki@outlook.com>
2024-07-01 11:03:25 +0100

    Client : Export Views

---
+++ Modules/MERN/Belt Exam/client/src/views/index.js
@@ -0,0 +1,3 @@
+export { default as Projects } from './Projects/Projects';
+export { default as NewProject } from './NewProject/NewProject';
+export { default as Errs } from './ErrsContainer/ErrsContainer';
