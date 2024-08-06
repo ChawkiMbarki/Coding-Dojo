31cfade406740a1bd428a8edd0a357b19385d8e6
ChawkiMbarki <mbarkishawki@outlook.com>
2024-08-06 15:44:51 +0100

    Used DOTNET 8.0.303 instead of 6.0.423

--- Modules/See Sharp/ASP.NET CORE/practice/Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Modules/See Sharp/ASP.NET CORE/practice/Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b4387840072b14e33bc3c764597b3a3c015a00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0039856bbb5a9ebcb85e04d0849006f4963100c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
