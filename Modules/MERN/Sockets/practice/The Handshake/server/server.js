00725a77d174125c73ef705957d40d7294230526
ChawkiMbarki <mbarkishawki@outlook.com>
2024-11-13 08:36:48 +0100

    Server: Installed nodemon and implemented cors

--- Modules/MERN/Sockets/practice/The Handshake/server/server.js
+++ Modules/MERN/Sockets/practice/The Handshake/server/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
 
 const server = app.listen(8000, () =>
   console.log("The server is all fired up on port 8000")
 );
 
-const io = require("socket.io")(server, { cors: true });
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
 
 io.on("connection", (socket) => {
   console.log("Nice to meet you. (shake hand)");
