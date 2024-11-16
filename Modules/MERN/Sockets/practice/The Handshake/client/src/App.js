84ba5a3033ec9c871d068cbd6046b30f3fbb1035
ChawkiMbarki <mbarkishawki@outlook.com>
2024-11-16 10:14:58 +0100

    Client: Added routes to different pages

--- Modules/MERN/Sockets/practice/The Handshake/client/src/App.js
+++ Modules/MERN/Sockets/practice/The Handshake/client/src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import "./App.css";
 
-import Chat from './components/Chat';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+import Chat from "./components/Chat";
+import Home from "./components/Home";
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
-    <Chat />
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home name={name} setName={setName} />}
+        />
+        <Route
+          path="/Chat"
+          element={<Chat name={name} />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
